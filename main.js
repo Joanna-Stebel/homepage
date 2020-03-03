28fef53d3ab96f0022479d27b73a4e94f0c7f338
Joanna Stebel <asiastebel@gmail.com>
2020-03-03 22:12:44 +0100

    initial comit

---
+++ main.js
@@ -0,0 +1,13 @@
+//console.log('siema');
+//const name = 'Joanna';
+//const age = '29';
+
+//console.log(name);
+//console.log(age);
+
+
+//console.log(`Nazywam się ${name} i mam ${age} lat`);
+
+const hello = 'Cześć! Fajnie cię widzieć ;P';
+
+alert(hello);
